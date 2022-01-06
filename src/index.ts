b8a0f0378ef85e84ab264fd085393ff0af45bd2e
EnzoSives <73887784+EnzoSives@users.noreply.github.com>
2022-01-06 19:54:31 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+//Mercado
+console.log("Almacen Enzo")
+console.log("Productos disponibles")
+console.log("Gaseosa - 50")
+console.log("Yerba - 20")
+console.log("Pure de tomate - 33")
+console.log("Harina - 40")
+console.log("Galletitas surtidas- 22")
+console.log("Fideos - 30")
+console.log("Arroz - 40")
+console.log("Azucar - 10")
+
+
